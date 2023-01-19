c3f95eff5779b917c6e78d4e682e77cde51e5981
Felix Lindner <felix@existenzquantor.info>
2023-01-19 10:48:10 +0100

    snake to kebab

--- src/ai_planner/core.clj
+++ src/ai_planner/core.clj
@@ -1,6 +1,6 @@
 (ns ai-planner.core
   (:require [clojure.data.json :as json])
-  (:require [pddl-parse-and-ground.core :refer [parse_and_ground]])
+  (:require [pddl-parse-and-ground.core :refer [parse-and-ground]])
   (:require [ai-planner.bfs :refer [best-first-search]])
   (:gen-class))
 
@@ -10,7 +10,7 @@
         :else (complete-initial-state initial-state (rest ground-predicates))))
 
 (defn -main [& args]
-  (let [parsed (parse_and_ground (first args) (second args))]
+  (let [parsed (parse-and-ground (first args) (second args))]
     (json/pprint (best-first-search
               (complete-initial-state (get-in parsed [:PDDLProblem :init]) (get-in parsed [:PDDLDomain :grounding :predicates]))
               (get-in parsed [:PDDLDomain :grounding :actions])
