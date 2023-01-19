7b82c55304a59d50854a95cbcc1538b6a1b35103
Felix Lindner <felix@existenzquantor.info>
2023-01-19 23:02:31 +0100

    fixed small bug in astar

--- src/ai_planner/astar.clj
+++ src/ai_planner/astar.clj
@@ -6,6 +6,16 @@
 (defn dist-to-goal [goal node]
   (assoc-in node [:h] (count (difference (set goal) (set (node :state))))))
 
+(defn find-state-in-parents [state current]
+  (cond
+    (nil? (current :action)) false
+    (nil? ((current :parent) :action)) (= state ((current :parent) :state)) 
+    (not= state ((current :parent) :state)) (find-state-in-parents state (current :parent))
+    :else true))
+
+(defn has-distinct-state? [node]
+  (not (find-state-in-parents (node :state) node)))
+ 
 (defn a-star-search-recur [actions goal queue]
   (if (seq queue)
     (let [node (first (sort #(< (+ (%1 :g) (%1 :h)) (+ (%2 :g) (%2 :h))) queue))
@@ -14,7 +24,8 @@
         {:plan (extract-plan node)}
         (if (seq applicable-actions)
           (let [new-nodes (map (partial apply-action node) applicable-actions)
-                new-nodes-with-g (map #(assoc-in %1 [:g] (+ 1 (get-in %1 [:parent :g]))) new-nodes)
+                new-nodes-distinct-state (filter has-distinct-state? new-nodes)
+                new-nodes-with-g (map #(assoc-in %1 [:g] (+ 1 (get-in %1 [:parent :g]))) new-nodes-distinct-state)
                 new-nodes-with-g-and-h (map (partial dist-to-goal goal) new-nodes-with-g)]
             (a-star-search-recur actions goal (concat (rest queue) new-nodes-with-g-and-h)))
           nil)))
