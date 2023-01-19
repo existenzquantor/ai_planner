49e6023e3d0171f16d453cb6730ae4fd7a4ade33
Felix Lindner <felix@existenzquantor.info>
2023-01-19 10:51:48 +0100

    version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/existenzquantor/ai_planner"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] [org.clojars.existenzquantor/pddl-parse-and-ground "0.1.9"]]
+  :dependencies [[org.clojure/clojure "1.11.1"] [org.clojars.existenzquantor/pddl-parse-and-ground "0.2.0"]]
   :main ^:skip-aot ai-planner.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
