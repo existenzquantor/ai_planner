cf44d922d20b79010eba46684da7c682dc9a8158
Felix Lindner <felix@existenzquantor.info>
2023-01-21 17:56:39 +0100

    some changes after experimenting

--- src/ai_planner/bfs.clj
+++ src/ai_planner/bfs.clj
@@ -1,17 +1,15 @@
 (ns ai-planner.bfs
-  (:require [clojure.set :refer [subset?]])
+  (:require [clojure.set :refer [subset? union]])
   (:require [ai-planner.general :refer[applicable? extract-plan formula-as-list apply-action has-distinct-state?]]))
 
-(defn best-first-search-recur [actions goal queue] 
-  (if (seq queue) 
+(defn best-first-search-recur [actions goal queue]
+  (when (seq queue)
     (let [node (first queue)
           applicable-actions (filter (partial applicable? (get-in node [:state])) actions)]
       (if (subset? (set goal) (set (get-in node [:state])))
         {:plan (extract-plan node)}
-        (if (seq applicable-actions)
-          (best-first-search-recur actions goal (concat (rest queue) (filter has-distinct-state? (map (partial apply-action node) applicable-actions))))
-          nil)))
-    nil))
+        (when (seq applicable-actions)
+          (best-first-search-recur actions goal (concat (rest queue) (filter has-distinct-state? (map (partial apply-action node) applicable-actions)))))))))
 
 (defn best-first-search [initial-state actions goal]
   (best-first-search-recur actions (formula-as-list goal) (list {:state initial-state :action nil :parent nil})))
--- src/ai_planner/core.clj
+++ src/ai_planner/core.clj
@@ -2,7 +2,6 @@
   (:require [clojure.data.json :as json])
   (:require [pddl-parse-and-ground.core :refer [parse-and-ground]])
   (:require [ai-planner.bfs :refer [best-first-search]])
-  (:require [ai-planner.astar :refer [a-star-search]])
   (:gen-class))
 
 (defn complete-initial-state [initial-state ground-predicates]
@@ -12,11 +11,7 @@
 
 (defn -main [& args]
   (let [parsed (parse-and-ground (first args) (second args))]
-    (cond (and (> (count args) 2) (= "astar" (nth args 2))) (json/pprint (a-star-search
-                                                                        (distinct (complete-initial-state (get-in parsed [:PDDLProblem :init]) (get-in parsed [:PDDLDomain :grounding :predicates])))
-                                                                        (get-in parsed [:PDDLDomain :grounding :actions])
-                                                                        (get-in parsed [:PDDLProblem :goal])))
-          :else (json/pprint (best-first-search
-                              (distinct (complete-initial-state (get-in parsed [:PDDLProblem :init]) (get-in parsed [:PDDLDomain :grounding :predicates])))
-                              (get-in parsed [:PDDLDomain :grounding :actions])
-                              (get-in parsed [:PDDLProblem :goal]))))))
+    (json/pprint (best-first-search
+                  (distinct (complete-initial-state (get-in parsed [:PDDLProblem :init]) (get-in parsed [:PDDLDomain :grounding :predicates])))
+                  (get-in parsed [:PDDLDomain :grounding :actions])
+                  (get-in parsed [:PDDLProblem :goal])))))
